fa8317088c794cefae21698d6bf7a65afc024aee
CHIKURA Shinsaku <scene.sc@gmail.com>
2009-12-06 23:04:27 +0900

    Add some tests.

--- test/test_ods_reader.rb
+++ test/test_ods_reader.rb
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
+
 require 'helper'
 
+require 'lib/ods_reader'
+
 class TestOdsReader < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def test_colreps
+    rows = []
+    ODSReader.open('test/files/colreps.ods', 'Sheet1') do |row|
+      rows << row
+    end
+    assert_equal [["abc", "いろは", "123", "", "", "", ""],
+      ["2009-12-06", "", "", "3", "5", "1", ""],
+      ["98", "5", "5", "5", "1", "1", "2"]], rows
   end
 end
